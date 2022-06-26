a5a9fa9d4f03977cebff3aee7bc16babd4f5bad3
jcchavez <juancarlos.chavez@gmail.com>
2022-06-26 00:38:39 -0500

    Complete REST API for Product Microservice

---
+++ src/main/java/com/ioSoftware/microservices/store/MicroservicesStoreApplication.java
@@ -0,0 +1,13 @@
+package com.ioSoftware.microservices.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesStoreApplication.class, args);
+	}
+
+}
