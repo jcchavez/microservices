b5ad2036bbc11608ad8431af6f9948d652b110a4
jcchavez <juancarlos.chavez@gmail.com>
2022-07-09 13:54:43 -0500

    enable eureka client sleut and actuator

--- src/main/java/com/ioSoftware/microservices/store/MicroservicesStoreApplication.java
+++ src/main/java/com/ioSoftware/microservices/store/MicroservicesStoreApplication.java
@@ -2,11 +2,14 @@ package com.ioSoftware.microservices.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MicroservicesStoreApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.cloud.bootstrap.enabled","true");
 		SpringApplication.run(MicroservicesStoreApplication.class, args);
 	}
 
